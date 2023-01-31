33603d95b3123aa786ef96b900d370b5a69d0b6a
guybt <guybt@mail.uc.edu>
2023-01-31 13:16:39 -0500

    Lab 3

---
+++ powershell/Braidenguy-sysinfo.ps1
@@ -0,0 +1,17 @@
+﻿function getIP{
+    (get-netipaddress).ipv4address | Select-String "10.*"
+}
+
+$IP = getIP
+
+$User = $env:UserName
+
+$Hostname = $env:computername
+
+$Version = $HOST.Version.Major
+
+$Date = Get-Date -UFormat "%A, %B %d %Y"
+
+$BODY = "This machine's IP is $IP. User is $User. Hostname is $Hostname. Powershell Version is $Version. Today's Date is $Date"
+
+Send-MailMessage -To "guybt@mail.uc.edu" -From "guybt@mail.uc.edu" -Subject "Braiden Guy IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
