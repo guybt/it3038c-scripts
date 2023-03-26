bd92345e40aeaa1ab5da23d2d00dc24831491a03
guybt <guybt@mail.uc.edu>
2023-03-26 15:32:05 -0400

    lab 8 correction

--- labs/lab8.py
+++ labs/lab8.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-data = requests.get('https://www.youtube.com/watch?v=hJ_mOKwAm3M')
+data = requests.get('https://winning-homebrew.com/make-hooch-at-home.html')
 soup = BeautifulSoup(data.content, "html.parser")
 
 title = soup.find('title').text.strip()
 
-print('The title of this youtube video is: ' + title)
+print('The title of this article is: ' + title)
