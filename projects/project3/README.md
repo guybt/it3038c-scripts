2a64990f6ca310b92d1487b2544602a597208311
guybt <guybt@mail.uc.edu>
2023-04-12 18:49:35 -0400

    project 3 progress

---
+++ projects/project3/README.md
@@ -0,0 +1,15 @@
+# Braiden Guy IT3038c Scripts
+## Project 2
+For Project 3, I wrote a python script that prompts the user for 2 Pokemon to compare (Name, Pokedex Number, Types, and Weaknesses). 
+
+### Inspiration
+As a lifelong casual Pokemon player, I thought that it would be neat if I created a program that would help me during a playthrough of any Pokemon game. Although I have played almost every Pokemon game, I sometimes forget a Pokedex number (useful when trying to complete the Pokedex) and types/weaknesses (useful for determining if a move will be super effective, not very effective, etc against an opponent during battle). 
+I created this script to aid with determining these things on the fly (without having to use the internet)
+
+
+### How it Works
+This script uses the following Python Modules:
+JSON (Comes with Python)
+Requests (pip install requests)
+
+This script calls the pokeapi, which can be found at https://pokeapi.co/, to retrieve info about 2 user-selected pokemon. The name, pokedex number, types, and weaknesses of 2 pokemon are retrieved and output to the user.
---
+++ projects/project3/project3.py
@@ -0,0 +1,43 @@
+import json
+import requests
+
+def fetch_pokemon_data(pokemon):
+    """This function calls the PokeAPI and returns name, Pokedex number, types, and weaknesses of 2 user declared Pokemon (all generations)."""
+    r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon.lower()}")
+    if r.status_code == 404: # This error indicates that the name may have been misspelled
+        print("Pokemon not found. Check your spelling and try again.")
+        return None
+    else:
+        data = r.json()
+        name = data['name']
+        pokedex_number = data['id']
+        types = [type_data['type']['name'] for type_data in data['types']]
+        weaknesses = []
+        for type_data in data['types']:
+            r = requests.get(type_data['type']['url'])
+            type_info = r.json()
+            for weak_to in type_info['damage_relations']['double_damage_from']:
+                weaknesses.append(weak_to['name'])
+        return {
+            'name': name,
+            'pokedex_number': pokedex_number,
+            'types': types,
+            'weaknesses': weaknesses
+        }
+
+# Prompt user for Pokemon1 name then fetch/return info to user
+pokemon1 = input("Enter the first Pokemon name: ")
+pokemon1_data = fetch_pokemon_data(pokemon1)
+print("\nComparing Pokemon:")
+print("\nPokemon 1: " + pokemon1_data['name'])
+print("Pokedex Number:", pokemon1_data['pokedex_number'])
+print("Types:", ", ".join(pokemon1_data['types']))
+print("Weaknesses:", ", ".join(pokemon1_data['weaknesses']))
+
+# Prompt user for Pokemon2 name then fetch/return info to user
+pokemon2 = input("\nEnter the second Pokemon name: ")
+pokemon2_data = fetch_pokemon_data(pokemon2)
+print("\nPokemon 2: " + pokemon2_data['name'])
+print("Pokedex Number:", pokemon2_data['pokedex_number'])
+print("Types:", ", ".join(pokemon2_data['types']))
+print("Weaknesses:", ", ".join(pokemon2_data['weaknesses']))
